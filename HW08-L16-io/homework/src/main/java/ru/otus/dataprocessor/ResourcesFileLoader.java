ccdd7e5191f759e66713a002093307369d534163
Yuliya Yuzeeva <akuliniya@mail.ru>
2022-06-06 14:51:16 +0300

    HW08-L16-io

--- HW08-L16-io/homework/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
+++ HW08-L16-io/homework/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
@@ -1,13 +1,5 @@
 package ru.otus.dataprocessor;
 
-import ru.otus.json.xjson.JavaxJsonDemo;
-import jakarta.json.Json;
-import jakarta.json.JsonArray;
-import jakarta.json.JsonNumber;
-import jakarta.json.JsonObject;
-import jakarta.json.JsonString;
-import jakarta.json.JsonStructure;
-import jakarta.json.JsonValue;
 import ru.otus.model.Measurement;
 import java.util.List;
 
@@ -19,11 +11,6 @@ public class ResourcesFileLoader implements Loader {
     @Override
     public List<Measurement> load() {
         //читает файл, парсит и возвращает результат
-        try (var jsonReader = Json.createReader(JavaxJsonDemo.class.getClassLoader().getResourceAsStream(fileName)) {
-            JsonStructure jsonFromTheFile = jsonReader.read();
-            System.out.println("\n json from the file:");
-            System.out.println(jsonFromTheFile);
-            System.out.println("property:" + jsonFromTheFile.getValue("/firstName"));
         return null;
     }
 }
